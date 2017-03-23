d3528e0e7eba301a945302301439164a12500106
G. Chalauri <g.chalauri@mia.gov.ge>
2017-03-23 20:39:45 +0400

    executor that returns a result

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,74 @@ package main;
 
 import main.creating_thread_executor.Server;
 import main.creating_thread_executor.Task;
+import main.executors_that_returns_result.FactorialCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
 
 /**
  * Created by G.Chalauri on 3/23/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        creatingThreadExecutor();
+        //creatingThreadExecutor();
+        executorThatRetursResultExample();
+    }
+
+    private static void executorThatRetursResultExample() {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.
+                newFixedThreadPool(2);
+
+        List<Future<Integer>> resultList = new ArrayList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            Integer number = random.nextInt(10);
+
+            FactorialCalculator calculator = new FactorialCalculator(number);
+
+            Future<Integer> result = executor.submit(calculator);
+
+            resultList.add(result);
+        }
+
+        do {
+            System.out.printf("Main: Number of Completed Tasks: %d\n", executor.getCompletedTaskCount());
+
+            for (int i = 0; i < resultList.size(); i++) {
+                Future<Integer> result = resultList.get(i);
+                System.out.printf("Main: Task %d: %s\n", i, result.
+                        isDone());
+            }
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        } while (executor.getCompletedTaskCount() < resultList.size());
+
+        System.out.printf("Main: Results\n");
+        for (int i = 0; i < resultList.size(); i++) {
+            Future<Integer> result = resultList.get(i);
+            Integer number = null;
+            try {
+                number = result.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            System.out.printf("Main: Task %d: %d\n", i, number);
+        }
+
+        executor.shutdown();
     }
 
     private static void creatingThreadExecutor() {
--- src/main/creating_thread_executor/Server.java
+++ src/main/creating_thread_executor/Server.java
@@ -17,16 +17,24 @@ public class Server {
 
     public Server() {
         executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+
+        /*
+        If you send more tasks than the number of threads, the remaining tasks will be blocked until
+        there is a free thread to process them This method receives the maximum number of threads
+        as a parameter you want to have in your executor. In your case, you have created an executor
+        with five threads.
+         */
+        //executor=(ThreadPoolExecutor)Executors.newFixedThreadPool(5);
     }
 
     public void executeTask(Task task) {
         System.out.printf("Server: A new task has arrived\n");
         executor.execute(task);
-        System.out.printf("Server: Pool Size: %d\n",executor.
+        System.out.printf("Server: Pool Size: %d\n", executor.
                 getPoolSize());
-        System.out.printf("Server: Active Count: %d\n",executor.
+        System.out.printf("Server: Active Count: %d\n", executor.
                 getActiveCount());
-        System.out.printf("Server: Completed Tasks: %d\n",executor.
+        System.out.printf("Server: Completed Tasks: %d\n", executor.
                 getCompletedTaskCount());
     }
 
